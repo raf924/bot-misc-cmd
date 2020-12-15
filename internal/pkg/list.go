e0bc9a2af59c97566adad5d1be7d86d500eb8943
Rafaël Naciri <rafael.naciri.dev@gmail.com>
2020-12-15 23:54:43 +0100

    feat: add list command

---
+++ internal/pkg/list.go
@@ -0,0 +1,46 @@
+package pkg
+
+import (
+	"github.com/raf924/bot/api/messages"
+	"github.com/raf924/bot/pkg/bot"
+	"github.com/raf924/bot/pkg/bot/command"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"strings"
+)
+
+func init() {
+	bot.HandleCommand(&ListCommand{})
+}
+
+type ListCommand struct {
+	bot command.Executor
+	command.NoOpInterceptor
+}
+
+func (l *ListCommand) Init(bot command.Executor) error {
+	l.bot = bot
+	return nil
+}
+
+func (l *ListCommand) Name() string {
+	return "list"
+}
+
+func (l *ListCommand) Aliases() []string {
+	return []string{"l"}
+}
+
+func (l *ListCommand) Execute(command *messages.CommandPacket) ([]*messages.BotPacket, error) {
+	var users []string
+	for _, u := range l.bot.OnlineUsers() {
+		users = append(users, u.Nick)
+	}
+	return []*messages.BotPacket{
+		{
+			Timestamp: timestamppb.Now(),
+			Message:   strings.Join(users, ", "),
+			Recipient: command.GetUser(),
+			Private:   command.GetPrivate(),
+		},
+	}, nil
+}
